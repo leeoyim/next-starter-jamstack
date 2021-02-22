f9af825fe2428b063714c57c3cf4dc5328401cba
leeoysys <leeoyos@gmail.com>
2021-02-22 12:54:06 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,9 @@
-import React from 'react'
-import Head from 'next/head'
+import { builder, BuilderComponent } from '@builder.io/react'
 
-const Home = () => (
-  <div>
-    <h1>Next.js on the [JAMstack](https://jamstack.org)</h1>
+builder.init('2d2da80d210749608d6025730f5b6ff1')
 
-    <h3>Hooray 🎉 - you've built this with <a href="https://nextjs.org">Next.js</a>!</h3>
-
-    <style jsx>{`
-      :global(html,body) {
-        margin: 0;
-        padding: 0;
-        height: 100%;
-      }
-
-      :global(body) {
-        font-size: calc(10px + 1vmin);
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
-          'Droid Sans', 'Helvetica Neue', sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        background-color: #282c34;
-        color: white;
-      }
-
-      a {
-        color: pink;
-        text-decoration: none;
-      }
-
-      .content {
-        padding: 0 32px;
-      }
-    `}</style>
-  </div>
+export default const MyComponent = () => (
+  <BuilderComponent
+    model="page"
+          entry="0f5967db882144119e581e843da33ac5" />
 )
-
-export default Home
